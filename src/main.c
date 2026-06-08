12136d15e6f8c6c82d87eeebe969ad87061a56ac
Vaishakh GK <vaishakh@vaishakhgk.com>
2026-06-09 01:50:06 +0530

    feat: Continuously read user commands until exit

--- src/main.c
+++ src/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    char *buff;
-    long unsigned int size = 0;
+    char *buff = NULL;
+    size_t size = 0;
 
     printf("ash$ ");
-    getline(&buff, &size, stdin);
+    while((getline(&buff, &size, stdin)) != -1) {
+        printf("%s", buff);
 
-    printf("%s", buff);
+        if(strcmp(buff, "exit\n") == 0) {
+            break;
+        }
+
+        printf("ash$ ");
+    }
+
+    free(buff);
 }
