d1a18517714aef695fb8d6c0e4f8f2cdc67cbc18
Vaishakh GK <vaishakh@vaishakhgk.com>
2026-06-13 21:35:46 +0530

    feat: Use custom parser without strtok

--- src/parser.c
+++ src/parser.c
@@ -4,28 +4,50 @@
 char **parse(char *line) {
     line[strcspn(line, "\n")] = '\0';
 
-    int size = 5;
+    int size = 10;
+    int token_size = 100;
     int argc = 0;
+    int pos = 0;
+
     char **argv = malloc(size * sizeof(char*));
+    char *token = malloc(token_size * sizeof(char));
 
-    char *token = strtok(line, " ");
+    char *c = line;
 
-    if(token == NULL) {
-        return NULL;
-    }
+    while(*c != '\0') {
+        if(*c == ' ') {
+            if(pos > 0) {
+                token[pos++] = '\0';
 
-    while(token != NULL) {
-        if(argc >= size - 1) {
-            size *= 2;
-            argv = realloc(argv, size * sizeof(char*));
+                if(argc >= size - 1) {
+                    size *= 2;
+                    argv = realloc(argv, size * sizeof(char*));
+                }
+
+                argv[argc++] = strdup(token);
+                pos = 0;
+            }
+
+            c++;
+            continue;
         }
 
-        argv[argc++] = token; 
-        
-        token = strtok(NULL, " ");
+        if(pos >= token_size - 1) {
+            token_size *= 2;
+            token = realloc(token, token_size * sizeof(char));
+        }
+
+        token[pos++] = *c;
+        c++;
+    }
+
+    if (pos > 0) {
+        token[pos] = '\0';
+        argv[argc++] = strdup(token);
     }
 
     argv[argc++] = NULL; 
+    free(token);
 
     return argv;
 }
